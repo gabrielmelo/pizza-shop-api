06d15df238f900269c02b14cab6c0b126d537472
Gabriel Melo <gabriel_smelo@proton.me>
2024-12-04 14:10:56 -0300

    feat: create seed

---
+++ src/db/connection.ts
@@ -0,0 +1,12 @@
+import { drizzle } from 'drizzle-orm/postgres-js'
+
+import * as schema from './schema'
+import postgres from 'postgres'
+
+import { env } from '../env'
+
+const connection = postgres(env.DATABASE_URL)
+
+export const db = drizzle(connection, {
+    schema 
+})
