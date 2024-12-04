b35a43a3bac1111efe90b5181fd584fdcb091a63
Gabriel Melo <gabriel_smelo@proton.me>
2024-12-04 13:51:26 -0300

    feat: configuration environment variables

--- src/db/migrate.ts
+++ src/db/migrate.ts
@@ -2,11 +2,15 @@ import postgres from 'postgres'
 
 import { drizzle } from 'drizzle-orm/postgres-js'
 import { migrate } from 'drizzle-orm/postgres-js/migrator'
+import { env } from '../env'
+import chalk from 'chalk'
 
-const connection = postgres('postgresql://docker:docker@localhost:5432/pizzashop', { max: 1 })
+const connection = postgres(env.DATABASE_URL, { max: 1 })
 const db = drizzle(connection)
 
 await migrate(db, { migrationsFolder: 'drizzle' })
 await connection.end()
+
+console.log(chalk.greenBright('Migrations applied successfully!'))
 
 process.exit()
